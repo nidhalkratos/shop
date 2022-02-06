248c2c85168d9af0f9a7651a34aea5ed80ca38cf
Nidhal Abidi <nidhalkratos@gmail.com>
2022-02-06 19:08:19 +0100

    Improves the Product page

---
+++ src/pages/components/ColorPicker.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Color</h1>
+        <div class="mt-4 flex space-x-4">
+            <button @click="setColor(color)" v-for="color in colors" :key="color" :style="{backgroundColor : color}" :class="{'ring' : color == selectedColor}" class="w-10 h-10 rounded-full ring-black ring-offset-2"></button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        colors : Array
+    },
+    data(){
+        return {
+            selectedColor : null,
+        }
+    },
+    methods:{
+        setColor(color){
+            this.selectedColor = color;
+            this.$emit('update:modelValue', color)
+        }
+    }
+}
+</script>
---
+++ src/pages/components/SizeSelector.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Size selection goes here
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
