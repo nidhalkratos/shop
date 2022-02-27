b76d837a7c58d0ad4b02a0b7eebcd07d8527ac55
Nidhal Abidi <nidhalkratos@gmail.com>
2022-02-27 18:14:44 +0100

    Installs tailwindcss

--- src/main.js
+++ src/main.js
@@ -7,11 +7,12 @@ import App from './App.vue'
 import routes from './routes';
 
 import {createRouter,createWebHashHistory} from 'vue-router';
+import './assets/tailwind.css'
 
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
     routes, // short for `routes: routes`
   })
-  
+
 createApp(App).use(router).mount('#app')
